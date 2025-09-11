7c1e5fa8c84d3aaebf32941f24527ff4b5184eef
TheLostRuins <109196612+TheLostRuins@users.noreply.github.com>
2025-09-11 10:32:15 -0400

    Create notes.cpp

---
+++ @mod/notes.cpp
@@ -0,0 +1,21 @@
+class cfgMagazines
+{
+	class Mag_Saiga_Drum20Rnd;
+	class Xmas_Saiga_Drum: Mag_Saiga_Drum20Rnd
+	{
+		scope = 2;
+		displayName = "Xmas Saiga Drum Mag";
+		descriptionShort  = "A drum magazine for the Xmas Saiga.";
+		count = 40;
+		hiddenSelections[] = {
+			"camo"
+		};
+		hiddenSelectionsTextures[] = {
+			"Xmas_Magazines\Xmas_Saiga_Drum\drum_CO_0.paa"
+
+		};				
+		hiddenSelectionsMaterials[] = {
+			"Xmas_Magazines\Xmas_Saiga_Drum\drum_0.rvmat"
+		};
+	};
+};
